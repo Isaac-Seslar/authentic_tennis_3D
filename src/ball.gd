07bab38063140d66c5419a4ffdaa6dccb13067a5
Isaac Seslar <56139690+Isaac-Seslar@users.noreply.github.com>
2025-11-17 22:14:18 -0700

    ball explodes on landing

--- src/ball.gd
+++ src/ball.gd
@@ -1,11 +1,56 @@
 extends RigidBody3D
 
+var bounce_count = 0
+var has_exploded = false
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var gpu_particles_3d: GPUParticles3D = $GPUParticles3D
 
+func _ready():
+	position = Vector3(5, 14, 0)
+	body_entered.connect(_on_body_entered)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_body_entered(body):
+	if has_exploded:
+		return
+	
+	# Check if we hit the floor
+	if body.is_in_group("floor"):
+		print("floor")
+		bounce_count += 1
+		print("Bounce #", bounce_count)
+		
+		if bounce_count >= 2:
+			explode()
+
+func explode():
+	has_exploded = true
+	$MeshInstance3D.visible = false
+	gpu_particles_3d.emitting = true
+	# Destroy nearby floor pieces
+	destroy_floor_at_position(global_position)
+	$Explosion_Timer.start()
+
+func destroy_floor_at_position(explosion_pos):
+	var radius_error = randf_range(0.5, 1)
+	var explosion_radius = 2.0 * radius_error
+	
+	# Find all floor tiles in the scene
+	var floor_tiles = get_tree().get_nodes_in_group("floor_tile")
+	
+	for tile in floor_tiles:
+		var distance = tile.global_position.distance_to(explosion_pos)
+		if distance < explosion_radius:
+			tile.queue_free()  
+			
+func reset_ball():
+	print("ball reset")
+	get_tree().reload_current_scene()
+	
+func _on_explosion_timer_timeout() -> void:
+	$Respawn_Timer.start()
+	queue_free()	
+	print("explode")
+
+func _on_respawn_timer_timeout() -> void:
+	reset_ball()
+	print("respawn") 

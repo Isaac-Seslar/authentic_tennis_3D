60ddb554374ba30b000573e6527ad255f5c97edc
Isaac Seslar <56139690+Isaac-Seslar@users.noreply.github.com>
2025-11-17 22:14:58 -0700

    Floor is generated in destructible pieces

---
+++ src/floor_generator.gd
@@ -0,0 +1,21 @@
+extends Node3D
+#
+#@export var floor_tile: PackedScene
+@export var grid_size = 152
+@export var tile_spacing = 0.25
+@export var floor_tile: PackedScene
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	generate_floor()
+
+
+func generate_floor():
+	
+	#var x=1
+	for x in range(-grid_size, 0):
+		for z in range(-grid_size-10, 0):
+			var tile = floor_tile.instantiate()
+			add_child(tile)
+			tile.position = Vector3(x * tile_spacing, 0, z * tile_spacing)
---
+++ src/floor_tile.gd
@@ -0,0 +1,6 @@
+extends StaticBody3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	add_to_group("floor")
+	add_to_group("floor_tile")

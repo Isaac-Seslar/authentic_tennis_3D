30b747c57c82bd3fb4e390df495f9a3936e9ac1a
Isaac Seslar <56139690+Isaac-Seslar@users.noreply.github.com>
2025-12-01 19:35:56 -0700

    Added spring arm camera and double jump

--- src/player.gd
+++ src/player.gd
@@ -2,17 +2,21 @@ extends CharacterBody3D
 
 
 const SPEED = 10.0
-const JUMP_VELOCITY = 10.0
+const JUMP_VELOCITY = 5.0
 const SWING_ROTATION = 65
 const SWING_SPEED = 0.2
 #cost SWING_RETURN = -
 
 @onready var arm_pivot: CharacterBody3D = $ArmPivot
 @onready var camera_pivot: Node3D = $CameraPivot
+@onready var camera: Camera3D = $Camera3D
+
+@export var tilt_limit = deg_to_rad(75)
 
 var is_swinging = false
 var mouse_sensitivity = 0.002
 var camera_x_rotation = 0.0
+var jumps = 0.0
 
 func _ready() -> void:
 	arm_pivot.rotation_degrees.x = 45
@@ -39,14 +43,23 @@ func swing_racket() -> void:
 	
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
+	if is_on_floor():
+		jumps=0
+	else:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+		jumps+=1
+		print("SINGLE JUMP")
+	elif Input.is_action_just_pressed("jump") and jumps==1 and not is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		jumps+=1
+		print("DOUBLE JUMP")
+	elif Input.is_action_just_pressed("jump") and jumps>=2:
+		pass
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "forward", "back")

161c4f7a0718394eb475589c9336b8031d95235f
Isaac Seslar <56139690+Isaac-Seslar@users.noreply.github.com>
2025-12-05 13:07:44 -0700

    Ball de-spawns when leaving the map

--- src/death_barrier.gd
+++ src/death_barrier.gd
@@ -15,7 +15,7 @@ func _on_body_entered(body:Node3D) -> void:
 	#if body.name =="RigidBody":
 		await get_tree().create_timer(1.0).timeout
 		get_parent().spawn_new_ball()
-		queue_free()
+		body.queue_free()
 		
 		print("new ball!")
 		
--- src/main.gd
+++ src/main.gd
@@ -4,7 +4,9 @@ extends WorldEnvironment
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var bus_idx = AudioServer.get_bus_index("Master")
+	
+	AudioServer.set_bus_mute(bus_idx, true)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.

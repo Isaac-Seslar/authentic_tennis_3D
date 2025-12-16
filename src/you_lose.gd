74c3ed7385d9b6aecc3b850129f42d62d761088a
Isaac Seslar <56139690+Isaac-Seslar@users.noreply.github.com>
2025-12-15 22:44:43 -0700

    Add restart button to lose screen

--- src/you_lose.gd
+++ src/you_lose.gd
@@ -1,4 +1,5 @@
 extends AnimatedSprite2D
+@onready var lose_button: Button = $"../lose_Button"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -6,8 +7,11 @@ func _ready() -> void:
 	modulate.a = 0.0
 	
 func show_lose_text() -> void:
-	position = get_viewport().size / 2
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
+	position = get_viewport().size / 4
+	lose_button.visible = true
+	lose_button.position = Vector2(get_viewport().size / 4) + Vector2(-36,100)
 	visible = true
 	var tween = create_tween()
 	tween.tween_property(self, "modulate:a", 1.0, 0.5)

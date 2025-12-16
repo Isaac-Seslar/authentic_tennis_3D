18760b42648144f6202e7353f39e74d811e00ddb
Isaac Seslar <56139690+Isaac-Seslar@users.noreply.github.com>
2025-12-15 22:45:04 -0700

    Add restart button to win screen

--- src/you_win.gd
+++ src/you_win.gd
@@ -1,4 +1,5 @@
 extends AnimatedSprite2D
+@onready var win_button: Button = $"../win_button"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -6,8 +7,11 @@ func _ready() -> void:
 	modulate.a = 0.0
 	
 func show_win_text() -> void:
-	position = get_viewport().size / 2
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
+	position = get_viewport().size / 2
+	win_button.position = Vector2(get_viewport().size / 2) + Vector2(-36,100)
+	win_button.visible = true
 	visible = true
 	var tween = create_tween()
 	tween.tween_property(self, "modulate:a", 1.0, 0.5)
